bc5ad69669d405680dc972d24af8cb6552f6962f
Andarov <bekturandarob678@gmail.com>
2022-09-30 16:21:13 +0500

    Switch player when number correct 0ne

--- src/input.js
+++ src/input.js
@@ -3,16 +3,30 @@ const elRetry = document.querySelector('#retry');
 const elGetPoint = document.querySelector('#get-point');
 const elGame = document.querySelector('#game');
 const elRandom = document.querySelector('#random');
-const elPlayerOne = document.querySelector('#player-one');
-const elPlayerTwo = document.querySelector('#player-two');
+const elPlayerOne = document.querySelector('#player-0');
+const elPlayerTwo = document.querySelector('#player-1');
 const elPlayerOneTotal = document.querySelector('#player-one-total');
 const elPlayerTwoTotal = document.querySelector('#player-two-total');
-const elPlayerOneCurrent = document.querySelector('#player-one-current');
-const elPlayerTwoCurrent = document.querySelector('#player-two-current');
+const elPlayerOneCurrent = document.querySelector('#current-0');
+const elPlayerTwoCurrent = document.querySelector('#current-1');
 
 // Current numbers
-// const currentOne = Number(elPlayerOneCurrent);
+let currentOne = 0;
+let activePlayer = 0;
 // const currentTwo = Number(elPlayerTwoCurrent);
 
 // Create random number
-let random = Math.trunc(Math.random() *6)+1;
+elGame.addEventListener('click', function() {
+    let random = Math.trunc(Math.random() *6)+1;
+    elRandom.textContent = random;
+    if(random != 1) {
+        currentOne += random;
+        document.getElementById(`current-${activePlayer}`).textContent = currentOne;
+    }else {
+        document.getElementById(`current-${activePlayer}`).textContent = 0;
+        currentOne = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        elPlayerOne.classList.toggle('bg-red-300');
+        elPlayerTwo.classList.toggle('bg-red-200');
+    }
+})
