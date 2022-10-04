ea8303abfdb7832ba9e5c1cc6b22b3f0b08d12a1
Andarov <bekturandarob678@gmail.com>
2022-10-04 14:10:13 +0500

    Add switch player and reset game

--- input.js
+++ input.js
@@ -5,28 +5,68 @@ const elGame = document.querySelector('#game');
 const elRandom = document.querySelector('#random');
 const elPlayerOne = document.querySelector('#player-0');
 const elPlayerTwo = document.querySelector('#player-1');
-const elPlayerOneTotal = document.querySelector('#player-one-total');
-const elPlayerTwoTotal = document.querySelector('#player-two-total');
+const elPlayerOneTotal = document.querySelector('#scores-0');
+const elPlayerTwoTotal = document.querySelector('#scores-1');
 const elPlayerOneCurrent = document.querySelector('#current-0');
 const elPlayerTwoCurrent = document.querySelector('#current-1');
 
 // Current numbers
+const scores = [0, 0]
 let currentOne = 0;
 let activePlayer = 0;
-// const currentTwo = Number(elPlayerTwoCurrent);
+let play = true;
+
+// Switch funksiyasi
+const switchPlayer = function() {
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
+    currentOne = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    elPlayerOne.classList.toggle('active');
+    elPlayerTwo.classList.toggle('active');
+}
 
 // Create random number
 elGame.addEventListener('click', function() {
-    let random = Math.trunc(Math.random() *6)+1;
-    elRandom.textContent = random;
-    if(random != 1) {
-        currentOne += random;
-        document.getElementById(`current-${activePlayer}`).textContent = currentOne;
-    }else {
-        document.getElementById(`current-${activePlayer}`).textContent = 0;
-        currentOne = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        elPlayerOne.classList.toggle('bg-red-300');
-        elPlayerTwo.classList.toggle('bg-red-200');
+    if(play){
+        let random = Math.trunc(Math.random() *6)+1;
+        elRandom.textContent = random;
+        if(random != 1) {
+            currentOne += random;
+            document.getElementById(`current-${activePlayer}`).textContent = currentOne;
+        }else {
+            switchPlayer()
+        }
     }
+})
+
+// Ballni olish
+elGetPoint.addEventListener('click', function() {
+    if(play) {
+        // Scoresni totalini ozgartirish
+        scores[activePlayer] += currentOne;
+        document.getElementById(`scores-${activePlayer}`).textContent = scores[activePlayer];
+    
+        if(scores[activePlayer] >= 30) {
+            play = false;
+            document.getElementById(`scores-${activePlayer}`).textContent = 'Player win';
+            document.getElementById(`player-${activePlayer}`).classList.add('winner');
+            elRandom.textContent = ''
+        }else {
+            elRandom.textContent = 0;
+            switchPlayer();
+        }
+    }
+})
+
+// Ynagilash
+elRetry.addEventListener('click', function() {
+    elRandom.textContent = 0;
+    elPlayerOneTotal.textContent = 0;
+    elPlayerTwoTotal.textContent = 0;
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
+    currentOne = 0;
+    elPlayerOne.classList.remove('winner')
+    elPlayerOne.classList.add('active')
+    elPlayerTwo.classList.remove('winner')
+    elPlayerTwo.classList.add('player-two')
 })
